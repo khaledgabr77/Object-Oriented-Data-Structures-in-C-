41dd1dbda8567c65c8d3ceafcda898587b3393dd
khaledgabr77 <khaledgabr77@gmail.com>
2022-07-10 18:30:31 +0400

    cpp contstractor examples

---
+++ Week-3/ccp-ctor/ex2/main.cpp
@@ -0,0 +1,23 @@
+#include "Cube.h"
+
+namespace uiuc {
+  Cube::Cube() {
+    length_ = 1;
+  }
+
+  Cube::Cube(double length) {
+    length_ = length;
+  }
+
+  double Cube::getVolume() {
+    return length_ * length_ * length_;
+  }
+
+  double Cube::getSurfaceArea() {
+    return 6 * length_ * length_;
+  }
+
+  void Cube::setLength(double length) {
+    length_ = length;
+  }
+}
---
+++ Week-3/ccp-ctor/ex3/Cube.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace uiuc {
+  class Cube {
+    public:
+      // No default constructor
+      Cube(double length);  // One argument constructor
+
+      double getVolume();
+      double getSurfaceArea();
+      void setLength(double length);
+
+    private:
+      double length_;
+  };
+}
